1563c4324e5e82a585e27fc07c62280c48098ac0
Jacob <1016417477@qq.com>
2021-02-27 15:47:10 +0800

    test

--- main.ts
+++ main.ts
@@ -1705,11 +1705,9 @@ namespace sensors {
         let arr = value1.split('')
         buf[0] = reg
         buf[1] = value
-        for (let i = 0; i < arr.length; i++) {
-            buf[1+i] = arr[i]
-        }
-        //let buf1 = buf.concat(arr)
-        //buf1[2+lengths] = lengths
+        // for (let i = 0; i < arr.length; i++) {
+        //     buf[1+i] = arr[i]
+        // }
         pins.i2cWriteBuffer(addr, buf)
     }
     
