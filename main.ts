3bcd088f46d04c32c47ca70fc7e79495b590d715
Jacob <1016417477@qq.com>
2021-03-02 09:25:22 +0800

    test

--- main.ts
+++ main.ts
@@ -1773,24 +1773,21 @@ namespace sensors {
         basic.pause(300)
     }
 
-    function stringToBytes ( str ) {  
+    function stringToBytes (str : string) {  
 
         var ch, st, re = []; 
         for (var i = 0; i < str.length; i++ ) { 
-            ch = str.charCodeAt(i);  // get char  
-            st = [];                 // set up "stack"  
+            ch = str.charCodeAt(i);  
+            st = [];                 
 
            do {  
-                st.push( ch & 0xFF );  // push byte to stack  
-                ch = ch >> 8;          // shift value down by 1 byte  
+                st.push( ch & 0xFF );  
+                ch = ch >> 8;          
             }    
 
             while ( ch );  
-            // add stack contents to result  
-            // done because chars have "wrong" endianness  
             re = re.concat( st.reverse() ); 
         }  
-        // return an array of bytes  
         return re;  
     } 
 
